547ad2d575249fadfecec9ed474973fc6d32255a
Alexandre Drouin <alexandre.drouin.8@ulaval.ca>
2017-04-17 17:02:30 -0400

    Smooth scrolling

--- graal.py
+++ graal.py
@@ -91,3 +91,7 @@ def hello():
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('page_not_found.html'), 404
+
+
+if __name__ == '__main__':
+      app.run(host='0.0.0.0', port=3333, debug=True)
