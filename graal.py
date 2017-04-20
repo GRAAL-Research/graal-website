9463541a097a4ed92e1c99dc73008ec3e285fcde
Alexandre Drouin <alexandre.drouin.8@ulaval.ca>
2017-04-20 15:14:25 -0400

    fixed michael

--- graal.py
+++ graal.py
@@ -238,7 +238,7 @@ def hello():
                  joined_year=2015,
                  face_picture="images/faces/mmorin.png",
                  linkedin="http://ca.linkedin.com/pub/michael-morin/24/418/154",
-                 scholar="https://scholar.google.ca/citations?user=X4pPn0YAAAAJ&hl",
+                 scholar="https://scholar.google.ca/citations?user=X4pPn0YAAAAJ",
                  website="http://www.michaelmorin.info")
     ]
 
