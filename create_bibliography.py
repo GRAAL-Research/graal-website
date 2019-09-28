006fe4fb4851fbe32b41295473f63c3230a48308
Frédérik Paradis <fredy_14@live.fr>
2019-09-28 17:46:36 -0400

    Use DBLP to retrieve profs' papers

--- create_bibliography.py
+++ create_bibliography.py
@@ -1,111 +1,79 @@
-#https://pypi.python.org/pypi/GoogleScholar
-# -*- coding: utf-8 -*-
-import scholarly
-import logging
-from collections import defaultdict
+import requests
+import xml.etree.ElementTree as ET
 import json
 import os
+import re
 
-import sys
-reload(sys)
-sys.setdefaultencoding('utf-8')
 
-PROFESSOR = ["Francois Laviolette", "Mario Marchand"]
+DBLP_URL = 'https://dblp.uni-trier.de/pers/xx/'
+
+PROFESSOR_DLPB_IDS = [
+    "l/Laviolette:Fran=ccedil=ois",
+    "m/Marchand:Mario",
+    "l/Lamontagne:Luc",
+    "k/Khoury:Richard",
+    "c/Corbeil:Jacques",
+    "d/Durand:Audrey"
+]
+
+
+def loadDBLPXML(dblp_id):
+    tmp_xml_filename = 'dblp.xml'
+    url = DBLP_URL + dblp_id
+    resp = requests.get(url)
+    return ET.fromstring(resp.content)
+
+def format_author_name(author):
+    last_space_pos = author.rfind(' ')
+    last_name = author[last_space_pos + 1:]
+    first_name_list = author[:last_space_pos].split(' ')
+    initials = ' '.join(first_name[0] + '.' for first_name in first_name_list)
+    return last_name + ' ' + initials
+
+def format_authors(authors_list):
+    authors = ', '.join(map(format_author_name, authors_list[:3]))
+    if len(authors_list) > 3:
+        authors += ' et al.'
+    return authors
+
+def get_value(publication_element, key, default=None):
+    el = publication_element.find(key)
+    return el.text if el is not None else default
+
+def create_bibliography_entry(key, publication_element):
+    title = get_value(publication_element, 'title')
+    year = int(get_value(publication_element, 'year'))
+    booktitle = get_value(publication_element, 'booktitle')
+    journal = get_value(publication_element, 'journal', default=booktitle)
+    volume = get_value(publication_element, 'volume')
+    pages = get_value(publication_element, 'pages')
+    url = pages = get_value(publication_element, 'ee')
+
+    authors_list = [el.text for el in publication_element.iter('author')]
+    authors = format_authors(authors_list)
+
+
+    citation = f"{authors} ({year}). {title} {journal}"
+    if volume is not None:
+        citation += f', {volume}'
+    if pages is not None:
+        citation += f', {pages}'
+    return {"year": year, "citation": citation, "url": url, "uniq_id": key}
 
 def create_bibliography():
-    if os.path.isfile("static/bib/bibliography_graal.json"):
-        logging.debug("Loading shelves")
-        bibliography = json.load(open("static/bib/bibliography_graal.json", 'r'))
-        logging.debug("Loaded %s papers already in the json file" % len(bibliography))
-    else:
-        bibliography = []
+    bibliography = []
+    seen_key = set()
+    for dblp_id in PROFESSOR_DLPB_IDS:
+        person_element = loadDBLPXML(dblp_id)
+        for publication_element in person_element.iter('r'):
+            publication_element = publication_element[0]
+            key = publication_element.attrib['key']
+            if key not in seen_key:
+                seen_key.add(key)
+                entry = create_bibliography_entry(key, publication_element)
+                bibliography.append(entry)
 
-    seen_id = defaultdict(bool)
-    for item in bibliography:
-        seen_id[item["uniq_id"]] = True
-
-    skipped_papers = open("Skipped_papers.txt", 'w')
-
-    for prof in PROFESSOR:
-        logging.debug("Searching for %s\n" % prof)
-        author_query = scholarly.search_author(prof)
-        logging.debug("Performed author search for %s\n" % prof)
-        author = next(author_query).fill()
-        logging.debug("Filled author\n")
-        for publication_short in author.publications:
-            print("Working on publication:")
-            print(publication_short)
-            try:
-               # 1. Do we already have the publication in the bibliography?
-                uniq_id = publication_short.id_citations
-                if uniq_id == "":
-                    print("id_citations is not a good uniq id")
-                    sys.exit(0)
-                if seen_id[uniq_id]:  # We already have this publication in the shelves.
-                    logging.debug("Publication with id %s was already in the shelves" % uniq_id)
-                    continue
-
-                print("Searching full entry for: %s\n"%publication_short.bib["title"])
-
-                # 2. Publication was not in the shelves. Search for the publication informations.
-                for publication_full in scholarly.search_pubs_query(publication_short.bib["title"]):
-                    print("one hit: %s" %publication_full)
-                    if publication_full.bib["title"] == publication_short.bib["title"]:
-                        publication_full.fill()
-                        break
-
-                if not publication_full._filled:
-                    print("Warning: Full citation for paper '%s' was not found" % publication_short.bib["title"])
-                    continue
-
-                title = publication_full.bib["title"]
-                try:
-                    authors = publication_full.bib["author"]
-                    journal = publication_full.bib.get("journal", publication_full.bib.get("booktitle"))
-                    year = publication_full.bib["year"]
-                except KeyError:
-                    # Major information missing, just skip it.
-                    skipped_papers.write("Can't find authors, journal or year for %s. Entry was skipped.\n" % title)
-                    continue
-
-                url = publication_full.bib.get("url", "")
-                volume = publication_full.bib.get("volume", "")
-                pages = publication_full.bib.get("pages", "")
-
-                #  url_scholar = publication_full.url_scholarbib  # The scholar url does not work (bcuz Google)
-
-                # 3. Format citation
-                citation = ""
-                for i in xrange(0, 3):
-                    try:
-                        a = authors.split(" and ")[i]
-                        if citation != "":
-                            citation += ", "
-                    except IndexError:
-                        # Less than 3 authors...
-                        break
-                    name = a.split(", ")[0]
-                    first_name = a.split(", ")[1]
-                    citation += "%s %s." % (name, first_name[0])
-                if len(authors.split(" and ")) > 3:
-                    citation += " et al."
-
-                citation += " (%s). %s. %s, %s, %s " % (year, title, journal, volume, pages)
-                citation = citation.replace("{\'e}", "é")
-
-                # 4. Add the citation to the bibliography...
-                logging.debug("Added: %s\n" % citation)
-                bibliography.append({"year": year, "citation": citation, "url": url, "uniq_id": uniq_id})
-                seen_id[uniq_id] = True   # We are only sure the paper was added in the bibliography at this point...
-            except:
-                print("If you see this repeatedly, you are probably punished by Google the god...")
-                json.dump(bibliography, open("static/bib/bibliography_graal_partial.json", 'w'))  # partial dump...
-                skipped_papers.write("Paper was not kept for an unknown error: %s\n" % publication_short.bib["title"])
-                continue
-    json.dump(bibliography, open("static/bib/bibliography_graal.json", 'w'))
-    skipped_papers.close()
+    json.dump(bibliography, open("static/bib/bibliography_graal.json", 'w'), indent=2)
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG,
-                        format="%(asctime)s.%(msecs)d %(levelname)s %(module)s - %(funcName)s: %(message)s")
     create_bibliography()
